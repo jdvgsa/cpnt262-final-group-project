a21051d54677c9add13c6e351c0cae4d53f6915a
jdvgsa <johnr.dagsa@outlook.com>
2023-11-15 12:37:51 -0700

    added logo to the header component

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,12 +1,12 @@
 <header class="flex bg-slate-800 items-center justify-between">
   <div class="ml-5 flex gap-5">
-  <h1 class="text-white font-bold  hover:underline cursor-pointer text-2xl">Hockey Gallery</h1>
-  <h2> Sample Logo </h2>
+  <h1 class="text-white font-bold  hover:underline cursor-pointer text-2xl mt-1">Anything</h1>
+  <img class="w-10" src="/images/LOGO.svg" alt="Site-Logo">
   </div>
   <nav class="p-5">
     <ul class="flex list-none m-0 p-0 gap-12">
       <li class="text-white font-bold  hover:underline cursor-pointer">Home</li>
-      <li class="text-white font-bold  hover:underline cursor-pointer">Contact</li>
+      <li class="text-white font-bold  hover:underline cursor-pointer">Gallery</li>
       <li class= "text-white font-bold  hover:underline cursor-pointer">About</li>
     </ul>
   </nav>
