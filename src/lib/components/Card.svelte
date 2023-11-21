632b47bf0248889fa7e174c4832df12a170d57d6
Brendan Malycky <bmalycky@hotmail.com>
2023-11-21 13:53:44 -0700

    changed prop name, data key

--- src/lib/components/Card.svelte
+++ src/lib/components/Card.svelte
@@ -3,11 +3,11 @@
   export let src;
   export let heading = "placeholder heading";
   export let description= "placeholder description";
-  export let href = "/"
+  export let slug = "/"
 </script>
 
 
-<a {href}>
+<a href={slug}>
   <article class="max-w-fit bg-slate-800 p-5 text-white rounded-xl hover:bg-slate-600 hover:scale-110 border-double border-slate-400 border-4">
     <img class="block w-56 h-56" {src} {alt}>
     <h2 class="text-2xl text-center my-4">{heading}</h2>
