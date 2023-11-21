e26738b4e9f51146bff284d829c1f705ebffb482
Brendan Malycky <bmalycky@hotmail.com>
2023-11-21 12:17:50 -0700

    added href key

--- src/lib/data.js
+++ src/lib/data.js
@@ -1,19 +1,19 @@
 export let galleryData = [
-  {src: "/images/canadiens.svg", heading: "Montreal Canadiens", description: "The Montreal Canadiens are a historic NHL team, founded in 1909.", alt: "nhl team logo image"},
+  {href: "/gallery/canadiens", src: "/images/canadiens.svg", heading: "Montreal Canadiens", description: "The Montreal Canadiens are a historic NHL team, founded in 1909.", alt: "nhl team logo image"},
 
-  {src: "/images/canucks.svg", heading: "Vancouver Canucks", description: "he Vancouver Canucks, established in 1970, bring a Pacific flair to the NHL.", alt: "nhl team logo image"},
+  {href: "/gallery/canucks", src: "/images/canucks.svg", heading: "Vancouver Canucks", description: "he Vancouver Canucks, established in 1970, bring a Pacific flair to the NHL.", alt: "nhl team logo image"},
 
-  {src: "/images/flames.svg", heading: "Calgary Flames", description: "The Calgary Flames, established in 1972, are an NHL team known for their fiery spirit.", alt: "nhl team logo image"},
+  {href: "/gallery/flames", src: "/images/flames.svg", heading: "Calgary Flames", description: "The Calgary Flames, established in 1972, are an NHL team known for their fiery spirit.", alt: "nhl team logo image"},
 
-  {src: "/images/jets.svg", heading: "Winnipeg Jets", description: "The Winnipeg Jets, reborn in 2011, bring NHL excitement to the heart of Canada.", alt: "nhl team logo image"},
+  {href: "/gallery/jets", src: "/images/jets.svg", heading: "Winnipeg Jets", description: "The Winnipeg Jets, reborn in 2011, bring NHL excitement to the heart of Canada.", alt: "nhl team logo image"},
 
-  {src: "/images/leafs.svg", heading: "Toronto Maple Leafs", description: "The Toronto Maple Leafs, dating back to 1917, stand as an enduring force in the NHL.", alt: "nhl team logo image"},
+  {href: "/gallery/leafs", src: "/images/leafs.svg", heading: "Toronto Maple Leafs", description: "The Toronto Maple Leafs, dating back to 1917, stand as an enduring force in the NHL.", alt: "nhl team logo image"},
 
-  {src: "/images/oilers.svg", heading: "Edmonton Oilers", description: "The Edmonton Oilers, founded in 1971, known for hockey greatness.", alt: "nhl team logo image"},
+  {href: "/gallery/oilers", src: "/images/oilers.svg", heading: "Edmonton Oilers", description: "The Edmonton Oilers, founded in 1971, known for hockey greatness.", alt: "nhl team logo image"},
 
-  {src: "/images/sabres.svg", heading: "Buffalo Sabres", description: "The Buffalo Sabres, founded in 1970, represent hockey pride in Buffalo, New York.", alt: "nhl team logo image"},
+  {href: "/gallery/sabres", src: "/images/sabres.svg", heading: "Buffalo Sabres", description: "The Buffalo Sabres, founded in 1970, represent hockey pride in Buffalo, New York.", alt: "nhl team logo image"},
 
-  {src: "/images/senators.svg", heading: "Ottawa Senators", description: "The Ottawa Senators, founded in 1992, represent Canada's capital in the NHL.", alt: "nhl team logo image"},
+  {href: "/gallery/senators", src: "/images/senators.svg", heading: "Ottawa Senators", description: "The Ottawa Senators, founded in 1992, represent Canada's capital in the NHL.", alt: "nhl team logo image"},
 
-  {src: "/images/wings.svg", heading: "Detroit Redwings", description: "The Detroit Red Wings, founded in 1926, they have 11 Stanley Cup victories.", alt: "nhl team logo image"}
+  {href: "/gallery/redwings", src: "/images/wings.svg", heading: "Detroit Redwings", description: "The Detroit Red Wings, founded in 1926, they have 11 Stanley Cup victories.", alt: "nhl team logo image"}
 ]
