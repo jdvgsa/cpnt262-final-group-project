d6a2923b4ed19e0e7371bb0c7e026006a9b2fef9
Brendan Malycky <bmalycky@hotmail.com>
2023-11-21 12:03:22 -0700

    moved header to layout, created gallery route

---
+++ src/routes/gallery/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+  import {galleryData} from "$lib/data.js"
+  import Card from "$lib/components/Card.svelte";
+</script>
+
+<main class="flex gap-x-36 gap-y-10 flex-wrap justify-center p-8">
+  {#each galleryData as galleryCard}
+    <Card {...galleryCard} />
+  {/each}
+</main>
