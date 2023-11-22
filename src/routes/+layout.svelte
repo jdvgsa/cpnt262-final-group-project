61980be8ab18d6fc62bd77acd9dfb99361d9b050
jdvgsa <johnr.dagsa@outlook.com>
2023-11-21 18:33:12 -0700

    moved Home component from layout to page.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,10 @@
 <script>
   import "../app.css";
   import Header from "../lib/components/Header.svelte";
-  import Home from "../lib/components/Home.svelte";
-  import Footer from "../lib/components/Footer.svelte";
+    import Footer from "../lib/components/Footer.svelte";
 </script>
 
 <Header />
-
-<Home />
 
 
 <slot />
