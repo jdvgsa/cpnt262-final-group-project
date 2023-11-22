43f6c26eba974034454639bb533390cd0a12cb76
Brendan Malycky <bmalycky@hotmail.com>
2023-11-21 20:07:22 -0700

    added more styles

--- src/routes/gallery/[slug]/+page.svelte
+++ src/routes/gallery/[slug]/+page.svelte
@@ -6,8 +6,8 @@
   <title>{data.card.heading}</title>
 </svelte:head>
 
-<main class="bg-gradient-to-br from-purple-600 to-blue-600">
-  <h1>{data.card.heading}</h1>
-  <img src={data.card.src} alt={data.card.alt}>
-  <p>{data.card.extendedInfo}</p>
+<main class="bg-gradient-to-br from-blue-800 to-white p-4">
+  <h1 class="text-5xl text-center mt-5">{data.card.heading}</h1>
+  <img class="mx-auto my-10" src={data.card.src} alt={data.card.alt}>
+  <p class="leading-8">{data.card.extendedInfo}</p>
 </main>
