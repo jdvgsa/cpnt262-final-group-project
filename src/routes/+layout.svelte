21447f42c63661df494fce167544551cd77b9793
jdvgsa <johnr.dagsa@outlook.com>
2023-11-21 18:13:59 -0700

    added background svg, created homepage component

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,13 @@
 <script>
   import "../app.css";
   import Header from "../lib/components/Header.svelte";
+  import Home from "../lib/components/Home.svelte";
   import Footer from "../lib/components/Footer.svelte";
 </script>
 
 <Header />
+
+<Home />
 
 <Footer />
 
