cc9fa43349e7acda70a80dacfe6f17307a9d7517
Brendan Malycky <bmalycky@hotmail.com>
2023-11-23 18:51:52 -0700

    added more data to style the dynamic routes

--- src/routes/gallery/[slug]/+page.svelte
+++ src/routes/gallery/[slug]/+page.svelte
@@ -10,5 +10,5 @@
   <h1 class="text-5xl text-center text-white mt-5">{data.card.heading}</h1>
   <img class="mx-auto my-10 lg:w-80 md:w-64 sm:w-56" src={data.card.src} alt={data.card.alt}>
   <p class="text-white text-2xl text-center mb-10">Stanley Cups: {data.card.cups}</p>
-  <p class="leading-8">{data.card.extendedInfo}</p>
+  <div class="leading-8 w-5/6 md:w-4/5 lg:w-3/5 mx-auto {data.card.textColor}">{@html data.card.extendedInfo}</div>
 </main>
