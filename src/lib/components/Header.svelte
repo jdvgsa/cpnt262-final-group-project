2e8ca406746ebafb0d2f1bbc1b2ca43277ae2c22
jdvgsa <johnr.dagsa@outlook.com>
2023-11-24 09:17:01 -0700

    fixed home component so that button is a clickable link that takes you to schedule, changed title from anything to Puck Poetry

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,7 +1,7 @@
 <header class="flex bg-slate-800 items-center justify-between">
   <div class="ml-5 flex gap-5">
-  <h1 class="text-white font-bold  hover:underline cursor-pointer text-2xl mt-1">Anything</h1>
-  <img class="w-10" src="/images/LOGO.svg" alt="Site-Logo">
+  <h1 class="text-white font-bold  hover:underline cursor-pointer text-2xl mt-1"><a href="/">Puck Poetry</a></h1>
+  <a href="/"><img class="w-10" src="/images/LOGO.svg" alt="Site-Logo"></a>
   </div>
   <nav class="p-5">
     <ul class="flex list-none m-0 p-0 gap-12">
--- src/lib/components/Home.svelte
+++ src/lib/components/Home.svelte
@@ -6,7 +6,7 @@
       <p class="pb-5 pt-3 max-w-prose mx-auto text-lg text-white">
           Welcome to our NHL fan hub, where the passion for hockey comes to life! The National Hockey League (NHL) is a professional ice hockey league featuring some of the most skilled and competitive teams from North America.</p>
       <p class="pb-5 pt-3 max-w-prose mx-auto text-lg text-white">Explore our website to stay updated on the latest scores, player stats, trade rumors, and everything else that makes the NHL a thrilling spectacle. Whether you're a seasoned hockey enthusiast or a newcomer to the sport, join us in celebrating the greatness of the NHL and the incredible athletes who make it unforgettable.</p>
-      <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">Get Started</button>
+      <a href="/schedule"><button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">Get Started</button></a>
   </div>
 </div>
 </section>
