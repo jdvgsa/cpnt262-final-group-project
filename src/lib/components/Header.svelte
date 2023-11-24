6c93bae6fced925fe6d806a5df3bad9c0a746e7a
jdvgsa <johnr.dagsa@outlook.com>
2023-11-24 08:58:41 -0700

    fixed gallery link in header component to link to our gallery page

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -6,7 +6,7 @@
   <nav class="p-5">
     <ul class="flex list-none m-0 p-0 gap-12">
       <li class="text-white font-bold  hover:underline cursor-pointer"><a href="/">Home</a></li>
-      <li class="text-white font-bold  hover:underline cursor-pointer">Gallery</li>
+      <li class="text-white font-bold  hover:underline cursor-pointer"><a href="/gallery">Gallery</a></li>
       <li class= "text-white font-bold  hover:underline cursor-pointer"><a href="/schedule">Games Schedule</a></li>
     </ul>
   </nav>
