aa6e6eb415f519d036a6b3e5b24ce25a11f78890
jdvgsa <johnr.dagsa@outlook.com>
2023-11-24 09:52:12 -0700

    optimized home component so that content doesnt overlap with footer in mobile view (added media query)

--- src/lib/components/Home.svelte
+++ src/lib/components/Home.svelte
@@ -1,12 +1,11 @@
-<section class="relative h-screen">
+<section class="relative min-h-screen">
   <div class="absolute inset-0 bg-cover bg-center" style="background-image: url(images/home2.jpg);"></div>
-<div class="absolute inset-0 bg-black opacity-75">
-  <div class="text-center">
-      <h1 class="pb-6 pt-3 text-6xl text-white">Want to learn more about Hockey?</h1>
-      <p class="pb-5 pt-3 max-w-prose mx-auto text-lg text-white">
-          Welcome to our NHL fan hub, where the passion for hockey comes to life! The National Hockey League (NHL) is a professional ice hockey league featuring some of the most skilled and competitive teams from North America.</p>
-      <p class="pb-5 pt-3 max-w-prose mx-auto text-lg text-white">Explore our website to stay updated on the latest scores, player stats, trade rumors, and everything else that makes the NHL a thrilling spectacle. Whether you're a seasoned hockey enthusiast or a newcomer to the sport, join us in celebrating the greatness of the NHL and the incredible athletes who make it unforgettable.</p>
-      <a href="/schedule"><button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">Get Started</button></a>
+  <div class="absolute inset-0 bg-black opacity-75">
+      <div class="text-center text-white p-8">
+          <h1 class="pb-6 pt-3 text-3xl md:text-6xl">Want to learn more about Hockey?</h1>
+          <p class="pb-5 pt-3 max-w-prose mx-auto text-md md:text-lg">Welcome to our NHL fan hub, where the passion for hockey comes to life! The National Hockey League (NHL) is a professional ice hockey league featuring some of the most skilled and competitive teams from North America.</p>
+          <p class="pb-5 pt-3 max-w-prose mx-auto text-md md:text-lg">Explore our website to stay updated on the latest scores, player stats, trade rumors, and everything else that makes the NHL a thrilling spectacle. Whether you're a seasoned hockey enthusiast or a newcomer to the sport, join us in celebrating the greatness of the NHL and the incredible athletes who make it unforgettable.</p>
+          <a href="/schedule"><button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">Get Started</button></a>
+      </div>
   </div>
-</div>
 </section>
